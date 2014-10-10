09392f16b54c091765c54bcdf60b7ba8fb96660e
Abel Miranda <abel.miranda@globant.com>
2014-10-09 23:10:44 -0300

    resources

--- catalogcabinets/src/main/java/de/joli/catalogcabinets/views/IWMultipleSelectorView.java
+++ catalogcabinets/src/main/java/de/joli/catalogcabinets/views/IWMultipleSelectorView.java
@@ -20,15 +20,26 @@ import de.joli.cataloglib.model.IWColor;
 public class IWMultipleSelectorView extends FrameLayout implements TabHost.TabContentFactory{
 
 
-    private ArrayList<IWColor>* items;
-    private  NSArray* filteredItems;
-    private  IWColor* selectedColor;
-    private  NSInteger selectedIndex;
     private id<IWMultipleSelectorViewControllerDelegate> delegate;
     private  NSString* propertyName;
     private  NSString* headerTitle;
     private  IWCabinet* cabinet;
     private  IWMultipleSelectorMode mode;
+
+    IBOutlet UIScrollView* scrollView;
+    IBOutlet UILabel* headerLabel;
+    IBOutlet UILabel *propertyNameView;
+    IBOutlet UIView *multipleContainer;
+    NSMutableArray* filteredList;
+    NSMutableArray* subviews;
+    IWColorsPanelView *panelColors;
+    IBOutlet UISwitch *switch1;
+    IBOutlet UISwitch *switch2;
+    IBOutlet UILabel *labelAllColors;
+    IBOutlet UILabel *labelAllColor1;
+    IBOutlet UILabel *labelColorDoor;
+    IBOutlet UIView *marker;
+    IBOutlet UIView *marker_back;
 
 
     private final View customView;
