f4855300238777452f79adcd70703cdfa200af5c
Abel Miranda <abel.miranda@globant.com>
2014-10-10 00:43:27 -0300

    key

--- cataloglib/src/main/java/de/joli/cataloglib/expansion/ExpansionDownloader.java
+++ cataloglib/src/main/java/de/joli/cataloglib/expansion/ExpansionDownloader.java
@@ -8,7 +8,7 @@ import com.google.android.vending.expansion.downloader.impl.DownloaderService;
 public class ExpansionDownloader extends DownloaderService {
     // You must use the public key belonging to your publisher account
     public static final int EXPANSION_VERSION = 2;
-    public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmAV0pAJ+fFqkXrferkbeBUx/jNRsKQYD1J3haVzpLUaUC/QREWLKqRJm85/TdjyN0fDM57eLjAUTtIY5bnEawbhPsiqjjWezbFaX1i0M4FKgvzGoNIaLrL99OiRTNOj6uCmpEwVVCQnN7yiqt/03l9VptUtpB4CslLQLpZhWh59ufhNS9V5U9IIJAQDNFwKQhMJbKF3dtrvr+14xRsVw92gs7twF9qQmYbs81YUsWDuG84oV86LMNzc9ht3c8MgO+xXgUnZVroLKln1RrN9oqQX5g4rL7t+kLTvH8yQK+zKxcX87GstOy25OIF7s7gSxke/1DkTGppRszaShE8kp5QIDAQAB";
+    public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsBcBd9w5q49EZpjAJf6MOIH3EyZbgreBgJJIRy4p+Brb1YY8wr+0FbdEyTF2nd3/8x8OB3DsB9vsy331bjLilGP9Z+M9nIEEkLydE5C+9ffq7EedStZ+HUc8vy94/42BXqXzODAAElYUYiaU0bOjZcWI0A++sbxAqN5Fedu/41OG8f0ez2PPC5JRnN/oWJjK+II9RvMrYobufKhtlcX8aw2T2DuPfieytY2l6mhGGPMdFaa4omMmJNj8imZgZPNrKRDE67M8D1XgG/kTg//HtP/p1+47k8NL5YxlpabvA4H0Au5F9jevwIJocvlX19dVO1ltGuMlvjqx8zPr6hN6DwIDAQAB";
     // You should also modify this salt
     public static final byte[] SALT = new byte[] { 1, 42, -12, -1, 54, 98,
             -100, -12, 43, 2, -8, -4, 9, 5, -106, -107, -33, 45, -1, 84
